7f95d316ab8bfedea1989f1eae48e1e124d42798
Louis Brent <louis_brent@Mac.lan>
2026-02-02 16:55:19 -0800

    Simplify function handler and sync with /api path

--- server/functions/api.js
+++ server/functions/api.js
@@ -1,11 +1,5 @@
 const serverless = require("serverless-http");
-const express = require("express");
 const app = require("../src/app");
 
-const wrapperApp = express();
-
-// We need to mount the app at the function path so that Express 
-// routing works correctly with the Netlify function URL structure.
-wrapperApp.use("/.netlify/functions/api", app);
-
-module.exports.handler = serverless(wrapperApp);
+// Export the serverless handler directly
+module.exports.handler = serverless(app);
